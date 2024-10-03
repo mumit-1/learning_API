192158d12c2eff1baffdb35dd85a011d50a4ed85
mumit-1 <mumitkhan85@gmail.com>
2024-10-03 11:39:22 +0600

    comments

---
+++ comments.js
@@ -0,0 +1,22 @@
+const firstFunction = () =>{
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('error happend',error));// to check the error
+    
+}
+
+//another way to write those fetch
+
+
+// for adding "async" infront of parameter we need to add "await" infront of them 
+const secondFunction = async()=>{
+    try{
+        const  res = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error('error happend')
+    }
+}
